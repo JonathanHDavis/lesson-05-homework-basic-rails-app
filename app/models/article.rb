b7affc64938d137e8d7a1d507eb3ea557f0517a3
Jonathan Davis <jonathanherscheldavis@gmail.com>
2016-02-29 20:58:46 -0500

    Attempt #!

---
+++ app/models/article.rb
@@ -0,0 +1,3 @@
+class Article < ActiveRecord::Base
+    # attr_accessor :title, :body
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+articles = Article.create([
+  {id: 1, title: "Test 1", body: "Body 1 blah blah blah"}
+  {id: 2, title: "Test 2", body: "Body 2 blah blah blah"}
+  {id: 3, title: "Test 3", body: "Body 3 blah blah blah"}
+  {id: 4, title: "Test 4", body: "Body 4 blah blah blah"}
+  {id: 5, title: "Test 5", body: "Body 5 blah blah blah"}
+  ])
